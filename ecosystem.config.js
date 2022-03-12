68f82dbbf58eb63a6faece185f76c707505d9b3f
June Hwang <zzang535@gmail.com>
2022-03-13 07:06:24 +0900

    ecosystem add

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    apps: [
+      {
+        name: 'NuxtAppName',
+        exec_mode: 'cluster',
+        instances: 'max', // Or a number of instances
+        script: './node_modules/nuxt/bin/nuxt.js',
+        args: 'start'
+      }
+    ]
+  }
