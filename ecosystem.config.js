cafb65199ddd1d918aaaf2c03b71164902f3b474
June Hwang <zzang535@gmail.com>
2022-04-10 19:15:14 +0900

    git command to sudo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     apps: [
       {
-        name: 'NuxtAppName',
+        name: 'marijuni',
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
